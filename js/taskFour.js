a159ac5effec0f80bb7d893a1caa5b3611820c9e
Bogdan Mylash <mangust@yahoo.com>
2017-09-22 18:02:38 +0300

    fix path task4

--- js/taskFour.js
+++ js/taskFour.js
@@ -9,7 +9,7 @@ let obj = {
 
 // MODEL
 function setBg(block, path) {
-    block.style.backgroundImage = "url('../" + path + "')";
+    block.style.backgroundImage = "url('" + path + "')";
     block.style.backgroundSize = "contain";
 }
 
@@ -19,6 +19,7 @@ obj.gallery.addEventListener('click', go);
 
 function go(e) {
     let path = e.target.dataset.path;
+    console.log(path);
     if (e.target.className == "gallery__img") {
         setBg(obj.bg, path);
     }
